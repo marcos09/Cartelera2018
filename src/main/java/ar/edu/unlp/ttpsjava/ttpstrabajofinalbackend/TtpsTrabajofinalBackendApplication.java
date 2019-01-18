7179ed684ab61306480dbb0ae3160920ffdfd020
Marcos Matias Bosnic <marcos@MacBook-Pro-de-Marcos.local>
2019-01-18 09:39:19 -0300

    First commit

---
+++ src/main/java/ar/edu/unlp/ttpsjava/ttpstrabajofinalbackend/TtpsTrabajofinalBackendApplication.java
@@ -0,0 +1,14 @@
+package ar.edu.unlp.ttpsjava.ttpstrabajofinalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtpsTrabajofinalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtpsTrabajofinalBackendApplication.class, args);
+	}
+
+}
+
